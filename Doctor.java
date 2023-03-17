717b8acdfa366ec4cfd8b94c96660db47566c1bc
ErenGunduz <akineren1121@hotmail.com>
2023-03-17 13:23:51 +0300

    .

--- Doctor.java
+++ Doctor.java
@@ -4,8 +4,21 @@ public class Doctor {
     int doctorID;
     int doctorExperiment;
 
-    Doctor(String doctorName){
+    Doctor(String doctorName, String doctorSpecialty,
+           int doctorID, int doctorExperiment){
         this.doctorName = doctorName;
+        this.doctorSpecialty = doctorSpecialty;
+        this.doctorID = doctorID;
+        this.doctorExperiment = doctorExperiment;
     }
+
+    void printInfos(){
+        System.out.println("Doctor's name: " + doctorName);
+        System.out.println("ID: " + doctorID);
+        System.out.println("Specialty: " + doctorSpecialty);
+        System.out.println("Experiment(Year): " + doctorExperiment);
+    }
+
+
 
 }
