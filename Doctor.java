d7785ac142df835d9b314c4012eeb4d5ac20aa19
ErenGunduz <akineren1121@hotmail.com>
2023-03-17 13:18:10 +0300

    .

---
+++ Doctor.java
@@ -0,0 +1,11 @@
+public class Doctor {
+    String doctorName;
+    String doctorSpecialty;
+    int doctorID;
+    int doctorExperiment;
+
+    Doctor(String doctorName){
+        this.doctorName = doctorName;
+    }
+
+}
