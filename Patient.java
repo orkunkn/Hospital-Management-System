4c8b33487c11d25935cd39ccd441a70b19a06ec4
ErenGunduz <akineren1121@hotmail.com>
2023-03-17 13:48:58 +0300

    .

--- Patient.java
+++ Patient.java
@@ -1,2 +1,43 @@
 public class Patient {
+    private String patientName;
+    private String patientIlness;
+    private int patientID;
+    // ilnessImportance, patient's
+    // ilness importance rating(0-5)
+
+    Patient(){}
+
+    private int ilnessImportance;
+
+    public String getPatientName() {
+        return patientName;
+    }
+
+    public String getPatientIlness() {
+        return patientIlness;
+    }
+
+    public int getPatientID() {
+        return patientID;
+    }
+
+    public int getIlnessImportance() {
+        return ilnessImportance;
+    }
+
+    public void setPatientName(String patientName) {
+        this.patientName = patientName;
+    }
+
+    public void setPatientIlness(String patientIlness) {
+        this.patientIlness = patientIlness;
+    }
+
+    public void setPatientID(int patientID) {
+        this.patientID = patientID;
+    }
+
+    public void setIlnessImportance(int ilnessImportance) {
+        this.ilnessImportance = ilnessImportance;
+    }
 }
