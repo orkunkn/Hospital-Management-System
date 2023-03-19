605b7be9b1b804c1b67f711d8b2c5a9cee02a2eb
orkunkn <orkunkaymann@gmail.com>
2023-03-19 23:45:46 +0300

    Rough design of entry GUI.

--- DoctorGUI.java
+++ DoctorGUI.java
@@ -1,3 +1,17 @@
-public class DoctorGUI {
+import javax.swing.*;
+import java.awt.*;
 
+public class DoctorGUI {
+    public static int width = 900;
+    public static int height = 900;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Doctor Page");
+        frame.setLocation(new Point(550, 250));
+        final JLabel label = new JLabel();
+        label.setBounds(0, 0, width, height);
+        frame.setSize(900, 900);
+        frame.setLayout(null);
+        frame.setVisible(true);
+    }
 }
--- EntryGUI.java
+++ EntryGUI.java
@@ -1,12 +1,33 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class EntryGUI {
-    public static void main(String args[]) {
-        JFrame frame = new JFrame("My First GUI");
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(900, 900);
-        JButton button1 = new JButton("Hello");
-        frame.getContentPane().add(button1);
+    public static int width = 800;
+    public static int height = 600;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("User Entry Page");
+        frame.setLocation(new Point(550, 250));
+        final JLabel label = new JLabel();
+        label.setBounds(0, 0, width, height);
+        final JPasswordField password = new JPasswordField();
+        password.setBounds(width / 2, 250, 200, 30);
+        JLabel labelUsername = new JLabel("Username:");
+        labelUsername.setBounds(width / 2 - 200, 200, width / 2, 30);
+        JLabel labelPassword = new JLabel("Password:");
+        labelPassword.setBounds(width / 2 - 200, 250, width / 2, 30);
+        JButton loginButton = new JButton("Login");
+        loginButton.setBounds(width / 2 - 50, height / 2 + 25, 80, 30);
+        final JTextField text = new JTextField();
+        text.setBounds(width / 2, 200, 200, 30);
+        frame.add(password);
+        frame.add(labelUsername);
+        frame.add(label);
+        frame.add(labelPassword);
+        frame.add(loginButton);
+        frame.add(text);
+        frame.setSize(width, height);
+        frame.setLayout(null);
         frame.setVisible(true);
     }
 }
